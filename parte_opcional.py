241d6e89d81eb2d3b750776e508421dde47fe56c
Ainhoa Díaz <ainhodia@ucm.es>
2023-03-12 10:53:08 +0100

    parte opcional final

--- parte_opcional.py
+++ parte_opcional.py
@@ -13,7 +13,7 @@ from multiprocessing import Array
 from time import sleep
 from random import random, randint
 
-N = 21
+N = 10
 K = 4
 NPROD = 3
 NCONS = 1
@@ -25,6 +25,8 @@ def delay(factor = 3):
 def add_data(storage,pid, data, mutex, productores):
     mutex.acquire()
     try:
+        #Tenemos que tener en cuenta a la hora de añadir elementos que en la posición en la que vamos a añadir 
+        #sea un -2 y además no se salga del búfer del productor. 
         for j in range(K*pid ,K*pid+K): 
             if storage[j] == -2: 
                 storage[j] = data 
@@ -39,6 +41,9 @@ def add_data(storage,pid, data, mutex, productores):
 def add_data1(storage,pid, data, mutex,productores): 
     mutex.acquire()
     try: 
+        #Se añade un -1 al final del búfer, en el primer elemento que sea -2, porque aunque el productor 
+        #deje de producir puede ser que tenga varios elementos ya producidos que haya consumir, por ello, 
+        #no podemos añadir el -1 en cualquier lugar, si no al final. 
         for j in range(K*pid ,K*pid+K): 
             if storage[j] == -2: 
                 storage[j] = -1
@@ -68,10 +73,13 @@ def producer(storage,empty, non_empty, mutex, productores):
 
 
 def consumer(storage,empty, non_empty, mutex, lista_cons, productores): 
+    i = 0
     for i in range(NPROD): 
         non_empty[i].acquire() 
-    while  storage[:] != [-1]*len(storage):
+    while  i < NPROD:
         i = 0
+        #El siguiente bucle es para comprobar cuando el primer elemento de cada bufer de cada productor es -1, 
+        #eso es lo que nos indicará cuando el proceso ha terminado. 
         for p in range(NPROD): 
             if storage[p*K] == -1 : 
                 i+=1
@@ -80,40 +88,30 @@ def consumer(storage,empty, non_empty, mutex, lista_cons, productores):
         minimo = 1000
         pos = 0 
         #No podemos usar la función mínimo para hallarlo puesto que si hay un -1 ese será el número que cogerá
-        #por tanto lo hacemos comprabando la lista entera y buscando el menor elemento.
-        for j in range(0,len(storage),K): 
+        #por tanto lo hacemos comprobando el primer elemento de cada bufer de cada productor, y comparándolos. 
+        for j in range(0,len(storage),K): #El range va de 0 a la longitud del storage con salto K (la longitud del búfer).
             if minimo > storage[j] and storage[j] >= 0 : 
                 minimo = storage[j] 
                 pos = j  
-                
-        print('storage', storage[:])
-        print('pos', pos)
-        print('minimo', minimo)
-        print('productores', productores[:])
         productor = productores[pos//K] 
-        print('productor', productor)
-        #Cuando consumimos un elemento, se queda vacío y se pone -2.
+        #Cuando consumimos un elemento, se queda vacío y se pone -2. En este caso, tenemos que mover todos los elementos 
+        #una posición hacia delante y poner el -2 al final del búfer del productor. 
         for l in range(pos,pos+K): 
             if l != pos+K-1: 
                 storage[l] = storage[l+1]  
             else: 
                 storage[l] = -2 
-          
-           
-        print('storage', storage[:])
-        #el problema esta ahi 
         empty[productor].release() 
         delay()  
         non_empty[productor].acquire() 
-        print('aqui')
         #Creamos una lista creciente donde almacenamos el productor que ha producido el valor minimo, y dicho valor. 
         lista_cons.append((productor, minimo)) 
         print(lista_cons)
         
 def main():
-    storage = Array('i',K*NPROD) 
+    storage = Array('i',K*NPROD) #Creamos el storage de K (longitud del bufer) * NPROD (número de productores).
     lista_cons = [] 
-    productores = Array('i',NPROD)#creo que tiene que ser K*NPROD
+    productores = Array('i',NPROD)
     for i in range(K*NPROD):
         storage[i] = -2
     print ("almacen inicial", storage[:])
@@ -123,9 +121,8 @@ def main():
     mutex = Lock()
     for i in range(N):
         non_empty.append(Semaphore(0))
-        empty.append(BoundedSemaphore(K))
+        empty.append(BoundedSemaphore(K)) #En este caso el BoundedSemaphore tiene que ser de tamaño K, para poder tener un búfer de tamaño K. 
      
-
     prodlst = [ Process(target=producer,
                         name=f'prod_{i}',
                         args=(storage,empty[i], non_empty[i], mutex, productores))
